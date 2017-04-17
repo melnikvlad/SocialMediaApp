039d7a12ef68065c914e26ecdaf17677a518f2b5
Vlad Melnikov <mvv.rnd21@mail.ru>
2017-04-17 21:55:00 +0300

    GoogleMaps#1
    - Map settup
    - View in fragment

--- Scruji/app/src/main/java/com/example/vlad/scruji/Fragments/Filter.java
+++ Scruji/app/src/main/java/com/example/vlad/scruji/Fragments/Filter.java
@@ -7,14 +7,47 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.vlad.scruji.R;
-
-/**
- * Created by Vlad on 23.02.2017.
- */
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Filter extends Fragment {
+    MapView mMapView;
+    private GoogleMap mGoogleMap;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_tab_filter,container,false);
+        View view = inflater.inflate(R.layout.fragment_tab_filter, container, false);
+        mMapView = (MapView) view.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mGoogleMap = googleMap;
+                goToLocation(-122.0840,37.4220,8);
+            }
+        });
         return view;
+    }
+
+    private void goToLocation(double lat, double lng,float zoom) {
+        LatLng coords = new LatLng(lat,lng);
+        CameraUpdate update  = CameraUpdateFactory.newLatLngZoom(coords,zoom);
+        mGoogleMap.moveCamera(update);
     }
 }
--- Scruji/app/src/main/java/com/example/vlad/scruji/MainActivity.java
+++ Scruji/app/src/main/java/com/example/vlad/scruji/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.vlad.scruji;
 
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.vlad.scruji.Constants.Constants;
 import com.example.vlad.scruji.Fragments.Login;
 import com.example.vlad.scruji.Fragments.MainScreen;
 import com.example.vlad.scruji.Fragments.CreateProfile;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 
 public class MainActivity extends FragmentActivity {
@@ -23,12 +27,29 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.activity_main);
         contextOfApplication = getApplicationContext();
         pref = getPreferences(0);
+        if(googlePlayServicesAvailiable()){
+            initFragment();
+        }
 
-        initFragment();
     }
 
     public static Context getContextOfApplication(){
         return contextOfApplication;
+    }
+
+    public boolean googlePlayServicesAvailiable(){
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailiable = api.isGooglePlayServicesAvailable(this);
+        if(isAvailiable == ConnectionResult.SUCCESS){
+            return true;
+        }else if(api.isUserResolvableError(isAvailiable)){
+            Dialog dialog = api.getErrorDialog(this,isAvailiable,0);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this,"Can't connect to services",Toast.LENGTH_LONG).show();
+        }
+        return false;
     }
 
     private void initFragment(){
